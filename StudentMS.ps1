6399b966e627356cc50e84ff056a2369aa6899a3
Tobyyyye <33171748+ZILINYE@users.noreply.github.com>
2023-02-15 15:20:34 -0500

    Add files via upload

---
+++ StudentMS.ps1
@@ -0,0 +1,25 @@
+Import-Module MSOnline
+$username = ""   # Administrator Username
+$unsecurepass = "" #Administrator Password 
+$password = ConvertTo-SecureString -AsPlainText $unsecurepass -Force
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $password
+Connect-MsolService -Credential $cred
+$StudentID = Read-Host "Please enter the Student ID 'W0XXXXXX' "
+$FirstName = Read-Host "Please enter the FirstName"
+$LastName = Read-Host "Please enter the LastName"
+$StudentDateOfBirth = Read-Host "Please enter the Student Date Of Birth <MMDD>"
+$PrincipalName = $StudentID + "@canadaacumen.ca"
+$DisplayName = $FirstName + " " + $LastName
+$fourID=$StudentID.Substring($StudentID.Length -4)
+$password = $fourID + $StudentDateOfBirth
+Write-Host "Student ID: "+$StudentID
+Write-Host "FirstName: "+$FirstName
+Write-Host "LastName: "+$LastName
+Write-Host "Password : "+$password
+$confirm = Read-Host "All the information Correct?"
+if ($confirm -eq 'y'){
+    $CreateUser = New-MsolUser -DisplayName $DisplayName -FirstName $FirstName -LastName $LastName -UserPrincipalName $PrincipalName -UsageLocation CA -LicenseAssignment AceAcumenAcademyCanada:STANDARDWOFFPACK_STUDENT -Password $password -StrongPasswordRequired $False -ForceChangePassword $False
+    Write-Host ("The Student account has been created")
+}
+
+
